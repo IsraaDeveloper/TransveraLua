496934a6871097b7fbb2b82a25ab9c3fc1e7e077
IsraaDeveloper <israadeveloper@gmail.com>
2026-02-17 22:08:26 +0700

    Upload Box_18/1509960.lua via Auto-Uploader

---
+++ Box_18/1509960.lua
@@ -0,0 +1,13 @@
+-- PICO​‌‌​​‌​​​​‌‌​​‌‌​‌‌​​‌​‌​‌‌​​​‌​​​‌‌​​​​​​‌‌​​​​​‌‌​​​‌​​​‌‌‌​​‌​‌‌​​‌​​​‌‌​​‌​​​​‌‌​‌​​​​‌‌​‌‌‌​‌‌​​‌​‌​​‌‌​‌​‌​‌‌​​‌​​​​‌‌​‌‌‌ PARK
+-- AppID 1509960 | Generated on 2026-02-15 04:04 UTC | openlua.cloud
+-- Note: This game supports Steam Workshop content
+
+-- Main Application
+addappid(1509960, 1, "de55873a706ae199d05ee85c55fa887e912a2d866128bbead08e8f5004c3c7d3")
+
+-- Content Depots (1)
+addappid(1509961, 1, "1ea9efc77f966369354189cff52ee9d118e406ff157718c1aa540beff716a879") -- PICO PARK Content
+
+-- Shared Depots (2)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (from 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (from 228980)
