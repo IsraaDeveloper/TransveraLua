177e0b46defccc0a1c17045e3d9d57a25b35865a
IsraaDeveloper <israadeveloper@gmail.com>
2026-02-17 21:58:34 +0700

    Upload Box_17/1369670.lua via Auto-Uploader

---
+++ Box_17/1369670.lua
@@ -0,0 +1,8 @@
+-- ============================================
+--  Credits: K3rnelPan1c (KernelOS / !K3rnalyze)
+--  Discord: https://discord.gg/k3rnalyze
+--  Website: https://kernelos.org/games
+-- ============================================
+
+addappid(1369670)
+addappid(1369671,0,"2997d5be0b53d13cde83335f41136a55c7198a843f319a2e9e3667ef228a1aed")
