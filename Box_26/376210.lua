63efab19a94a1048ad9d1801816d981adf315e4c
IsraaDeveloper <israadeveloper@gmail.com>
2026-02-18 09:42:12 +0700

    Upload Box_26/376210.lua via Auto-Uploader

---
+++ Box_26/376210.lua
@@ -0,0 +1,28 @@
+-- The​​‌‌​​​​​‌‌​​‌‌​​​‌‌​​‌‌​​‌‌​‌‌​​​‌‌​​‌‌​​‌‌​‌‌​​​‌‌​​​‌​​‌‌​​‌‌​‌‌​​‌‌​​​‌‌​​​‌​​‌‌‌​​‌​​‌‌​​​​​​‌‌​​‌​​​‌‌​​‌‌​​‌‌​​​​​‌‌​​​​‌ Isle
+-- AppID 376210 | Generated on 2026-02-16 14:17 UTC | openlua.cloud
+-- Warning: This game uses DRM (BattlEye)
+-- Note: This game supports Steam Workshop content
+
+-- Main Application
+addappid(376210, 1, "6070d44381d46f6e5add1c34b87b925c1cad06d0b8c5e4844139014c4244687f")
+
+-- Content Depots (2)
+addappid(376211, 1, "4cd41758f8182ab8aa5c2ae935995c9c9fd3406396541e245d988a47dcd5fe3b") -- The Isle - Content
+addappid(376212, 1, "ec98ff5da26fe8ed2ed0776bff3cf9b786d8942eafcf581c04401c7cf8621b54") -- The Isle Depot (Windows)
+
+-- Shared Depots (15)
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") -- VC 2005 Redist (from 228980)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") -- VC 2008 Redist (from 228980)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (from 228980)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (from 228980)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (from 228980)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (from 228980)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (from 228980)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (from 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (from 228980)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") -- .NET 4.5.2 Redist (from 228980)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- .NET 4.6 Redist (from 228980)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (from 228980)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (from 228980)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") -- OpenAL 2.0.7.0 Redist (from 228980)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") -- PhysX System Software 9.14.0702 (from 228980)
